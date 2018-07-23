131099cac8c49971ac9195896fa0c53c68729919
oddmunds <oddmunds@iterate.no>
2018-07-23 18:31:27 +0200

    mix new bevlahi

---
+++ lib/bavlahi.ex
@@ -0,0 +1,18 @@
+defmodule Bavlahi do
+  @moduledoc """
+  Documentation for Bavlahi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bavlahi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bavlahi_test.exs
@@ -0,0 +1,8 @@
+defmodule BavlahiTest do
+  use ExUnit.Case
+  doctest Bavlahi
+
+  test "greets the world" do
+    assert Bavlahi.hello() == :world
+  end
+end
