ebe52045fd9ba153d3189a56cb040e78e99e7a83
oddmunds <oddmunds@iterate.no>
2018-07-29 16:20:09 +0200

    Extract code from script to module

--- latest_video_by_channel_url.exs
+++ latest_video_by_channel_url.exs
@@ -1,36 +1,7 @@
 [channel_id | _] = System.argv()
 
-{:ok, token} = Goth.Token.for_scope("https://www.googleapis.com/auth/youtube")
-conn = GoogleApi.YouTube.V3.Connection.new(token.token)
+conn = Bavlahi.get_connection()
 
-{:ok, channels_list} =
-  GoogleApi.YouTube.V3.Api.Channels.youtube_channels_list(
-    conn,
-    "contentDetails",
-    id: channel_id
-  )
-
-uploads_id =
-  case channels_list.items do
-    [channel] ->
-      channel.contentDetails.relatedPlaylists.uploads
-
-    _ ->
-      exit(1)
-  end
-
-{:ok, playlistitem_list} =
-  GoogleApi.YouTube.V3.Api.PlaylistItems.youtube_playlist_items_list(
-    conn,
-    "contentDetails",
-    playlistId: uploads_id,
-    maxResults: 1
-  )
-
-video_id =
-  case playlistitem_list.items do
-    [playlistitem] -> playlistitem.contentDetails.videoId
-    _ -> exit(1)
-  end
+video_id = Bavlahi.get_latest_video_for_channel(conn, channel_id)
 
 IO.puts("https://www.youtube.com/watch?v=#{video_id}")
--- lib/bavlahi.ex
+++ lib/bavlahi.ex
@@ -15,4 +15,36 @@ defmodule Bavlahi do
   def hello do
     :world
   end
+
+  def get_connection do
+    {:ok, token} = Goth.Token.for_scope("https://www.googleapis.com/auth/youtube")
+    GoogleApi.YouTube.V3.Connection.new(token.token)
+  end
+
+  def get_latest_video_for_channel(conn, channel_id) do
+    {:ok, channels_list} =
+      GoogleApi.YouTube.V3.Api.Channels.youtube_channels_list(
+        conn,
+        "contentDetails",
+        id: channel_id
+      )
+
+    uploads_id =
+      case channels_list.items do
+        [channel] ->
+          channel.contentDetails.relatedPlaylists.uploads
+      end
+
+    {:ok, playlistitem_list} =
+      GoogleApi.YouTube.V3.Api.PlaylistItems.youtube_playlist_items_list(
+        conn,
+        "contentDetails",
+        playlistId: uploads_id,
+        maxResults: 1
+      )
+
+    case playlistitem_list.items do
+      [playlistitem] -> playlistitem.contentDetails.videoId
+    end
+  end
 end
