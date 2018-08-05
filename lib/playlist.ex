cbe260343da9a031c29723da68bf6ef7905b36bc
oddmunds <oddmunds@iterate.no>
2018-08-05 22:31:45 +0200

    move stuff to youtube

--- lib/playlist.ex
+++ lib/playlist.ex
@@ -10,13 +10,7 @@ defmodule Playlist do
 
   @spec get_newest(Playlist.t()) :: Video.t()
   def get_newest(playlist) do
-    {:ok, playlistitem_list} =
-      GoogleApi.YouTube.V3.Api.PlaylistItems.youtube_playlist_items_list(
-        Youtube.get_connection(),
-        "contentDetails",
-        playlistId: playlist.id,
-        maxResults: 1
-      )
+    {:ok, playlistitem_list} = Youtube.get_one_playlist_items(playlist.id)
 
     case playlistitem_list.items do
       [playlistitem] -> %Video{id: playlistitem.contentDetails.videoId}
--- lib/youtube.ex
+++ lib/youtube.ex
@@ -4,4 +4,13 @@ defmodule Youtube do
     {:ok, token} = Goth.Token.for_scope("https://www.googleapis.com/auth/youtube")
     GoogleApi.YouTube.V3.Connection.new(token.token)
   end
+
+  def get_one_playlist_items(playlist_id) do
+    GoogleApi.YouTube.V3.Api.PlaylistItems.youtube_playlist_items_list(
+      Youtube.get_connection(),
+      "contentDetails",
+      playlistId: playlist_id,
+      maxResults: 1
+    )
+  end
 end
